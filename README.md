a4af5d4b9133dd41523d8f3f9ee87a1a30ffd7b8
Darcy <yijianrouqing@gmail.com>
2016-09-11 19:38:13 +0800

    haha

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+ <learn python> 04E
 this is a exercise after class : word game.
+test require:hahaha
--- wordgame.py
+++ wordgame.py
@@ -1,8 +1,9 @@
 import random
 secret = random.randint(1,10)
-print('------------------我爱鱼C工作室------------------')
+print('----------------我爱鱼C工作室----------------')
 temp = input("不妨猜一下小甲鱼现在心里想的是哪个数字：")
 guess = int(temp)
+
 while guess != secret:
     temp = input("哎呀，猜错了，请重新输入吧：")
     guess = int(temp)
@@ -14,5 +15,6 @@ while guess != secret:
             print("哥，大了大了~~~")
         else:
             print("嘿，小了，小了~~~")
-print("游戏结束，不玩啦^_^")
 
+print("游戏结束，不玩啦^_^")
+print("change word")
